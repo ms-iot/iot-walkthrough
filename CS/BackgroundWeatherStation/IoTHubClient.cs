ccf460a093c04907a64b50427f60e2cd31c7a92a
Tiago Koji Castro Shibata <t-ticast@microsoft.com>
2017-02-17 18:04:36 -0800

    Use TPM keys to connect to Azure

--- CS/BackgroundWeatherStation/IoTHubClient.cs
+++ CS/BackgroundWeatherStation/IoTHubClient.cs
@@ -1,40 +1,71 @@
 ﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Exceptions;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.Devices.Tpm;
 using System;
 using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using Windows.Data.Json;
 
 namespace BackgroundWeatherStation
 {
     class IoTHubClient
     {
+        private TpmDevice _tpm = new TpmDevice(0);
         private DeviceClient _deviceClient;
+        private String _id;
 
         public IoTHubClient()
         {
-            _deviceClient = DeviceClient.Create(Keys.IOT_HUB_ENDPOINT, AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(Keys.DEVICE_ID, Keys.DEVICE_KEY));
-            _deviceClient.OpenAsync();
+            _id = _tpm.GetDeviceId();
+            RefreshToken();
         }
 
-        public async void LogData(double temperature, double humidity, double pressure)
+        public async void LogDataAsync(double temperature, double humidity, double pressure)
         {
             var messageString = new JsonObject
             {
                 { "currentTemperature", JsonValue.CreateNumberValue(temperature) },
                 { "currentHumidity", JsonValue.CreateNumberValue(humidity) },
                 { "currentPressure", JsonValue.CreateNumberValue(pressure) },
-                { "deviceId", JsonValue.CreateStringValue(Keys.DEVICE_ID) },
+                { "deviceId", JsonValue.CreateStringValue(_id) },
                 { "time", JsonValue.CreateStringValue(DateTime.Now.ToString()) },
             }.Stringify();
             var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
             try
             {
-                await _deviceClient.SendEventAsync(message);
+                try
+                {
+                    await _deviceClient.SendEventAsync(message);
+                }
+                catch (UnauthorizedException)
+                {
+                    Debug.WriteLine("Azure UnauthorizedException, refreshing SAS token");
+                    await RefreshToken();
+                    await _deviceClient.SendEventAsync(message);
+                }
             }
             catch (Exception e)
             {
                 Debug.WriteLine("Error logging data to Azure:\n" + e.Message);
             }
+        }
+
+        private async Task RefreshToken()
+        {
+            var method = AuthenticationMethodFactory.CreateAuthenticationWithToken(_id, _tpm.GetSASToken());
+            _deviceClient = DeviceClient.Create(_tpm.GetHostName(), method, TransportType.Mqtt);
+            await _deviceClient.SetDesiredPropertyUpdateCallback(OnDesiredPropertyChanged, null);
+            var twin = await _deviceClient.GetTwinAsync();
+            Debug.WriteLine(twin.ToJson());
+        }
+
+        private async Task OnDesiredPropertyChanged(TwinCollection desiredProperties, object userContext)
+        {
+            Debug.WriteLine("Property change:");
+            Debug.WriteLine(desiredProperties.ToJson());
         }
     }
 }
