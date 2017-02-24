3d193decf7a81567722364b89b97ec2d421064d4
Tiago Koji Castro Shibata <t-ticast@microsoft.com>
2017-02-24 14:37:36 -0800

    Update PFN, simplify EventHandler invocation

--- CS/ShowcaseBridgeService/AppServiceConnectionFactory.cs
+++ CS/ShowcaseBridgeService/AppServiceConnectionFactory.cs
@@ -8,7 +8,7 @@ namespace ShowcaseBridgeService
         {
             AppServiceConnection connection = new AppServiceConnection();
             connection.AppServiceName = "com.microsoft.showcase.bridge";
-            connection.PackageFamilyName = "05479d55-aff3-469f-a7d5-bf38a8aeb3f5_b8fas1k6xztg8";
+            connection.PackageFamilyName = "19434TiagoShibata.devex-showcase_gr440wvt0bh62";
             return connection;
         }
     }
