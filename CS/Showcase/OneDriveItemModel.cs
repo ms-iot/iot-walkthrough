ba4d5f55b4f95d771f10cee7b7cef079463b1278
Tiago Koji Castro Shibata <t-ticast@microsoft.com>
2017-02-24 17:13:01 -0800

    OneDrive integration, support dynamic resizing in SlideShow, strech controls

---
+++ CS/Showcase/OneDriveItemModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OneDrive.Sdk;
+using System.ComponentModel;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Showcase
+{
+    public class OneDriveItemModel : INotifyPropertyChanged
+    {
+        private BitmapSource bitmap;
+
+        public OneDriveItemModel(Item item)
+        {
+            this.Item = item;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return this.Item == null ? null : this.Item.Id;
+            }
+        }
+
+        public Item Item { get; private set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.Item.Name;
+            }
+        }
+
+        //INotifyPropertyChanged members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
--- CS/Showcase/Views/SlideShow.xaml.cs
+++ CS/Showcase/Views/SlideShow.xaml.cs
@@ -1,49 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using Windows.Graphics.Display;
+using System.IO;
+using System.Threading.Tasks;
 using Windows.System.Threading;
+using Windows.UI;
 using Windows.UI.Core;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace Showcase
 {
     public sealed partial class SlideShow : Page
     {
-        private readonly String[] imageList = { "http://openwalls.com/image/11878/windows_background_1920x1200.jpg", "http://wallpaperswide.com/download/windows_10_hero_4k-wallpaper-2880x1800.jpg" };
         private int currentImage = 0;
         private int imageTime = 10000;
         private ThreadPoolTimer startFadeTimer;
         private CoreDispatcher uiDispatcher;
+        private OneDriveItemController _oneDrive = new OneDriveItemController();
+        private List<OneDriveItemModel> _images;
 
         public SlideShow()
         {
             this.InitializeComponent();
             uiDispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+            SetBackground(Color.FromArgb(255, 255, 255, 255));
+        }
 
-            // FIXME handle dynamic resizing
-            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
-            var scaleFactor = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
-            var width = bounds.Width * scaleFactor;
-            var height = bounds.Height * scaleFactor;
-            ImageGrid.Width = width;
-            ImageGrid.Height = height;
-            //ForegroundImage.RenderTransformOrigin = new Windows.Foundation.Point(width / 2, height / 2);
-            Debug.WriteLine("Resolution: " + width + "x" + height);
-            BackgroundImage.Source = new BitmapImage(new Uri(imageList[0]));
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            await _oneDrive.InitAsync();
+            _images = await _oneDrive.GetImagesAsync(null);
+            BackgroundImage.Source = await LoadImage(_images[0].Id);
             Play();
         }
 
         private async void StartFadeAnimation(ThreadPoolTimer timer = null)
         {
-            await uiDispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            await uiDispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
             {
                 ForegroundImage.Source = BackgroundImage.Source;
                 ForegroundImage.Opacity = 1;
-                currentImage = (currentImage + 1) % imageList.Length;
-                BackgroundImage.Source = new BitmapImage(new Uri(imageList[currentImage]));
+                currentImage = (currentImage + 1) % _images.Count;
+                BackgroundImage.Source = await LoadImage(_images[currentImage].Id);
                 SlideShowFade.Begin();
             });
         }
@@ -81,8 +82,7 @@ namespace Showcase
 
         private void SetTime_Click(object sender, RoutedEventArgs e)
         {
-            TimeDropdown.IsOpen = true;
-            Debug.WriteLine(TimeDropdown.IsOpen);
+            SetTimePopup.IsOpen = true;
         }
 
         private void SetTimePopup_ItemClick(object sender, ItemClickEventArgs e)
@@ -93,7 +93,7 @@ namespace Showcase
             {
                 case "Faster transition":
                     Debug.WriteLine("Prev fade time " + FadeAnimation.Duration.TimeSpan.Milliseconds);
-                    FadeAnimation.Duration = TimeSpan.FromMilliseconds(FadeAnimation.Duration.TimeSpan.TotalMilliseconds / 2);
+                    ForegroundFadeAnimation.Duration = FadeAnimation.Duration = TimeSpan.FromMilliseconds(FadeAnimation.Duration.TimeSpan.TotalMilliseconds / 2);
                     Debug.WriteLine("Fade time " + FadeAnimation.Duration.TimeSpan.Milliseconds);
                     break;
 
@@ -108,6 +108,68 @@ namespace Showcase
                     Debug.WriteLine("Unknown popup option " + item.Text + "selected");
                     break;
             }
+        }
+
+        private void SetStretchType_Click(object sender, RoutedEventArgs e)
+        {
+            SetStretchTypePopup.IsOpen = true;
+        }
+
+        private void SetStretchTypePopup_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            TextBlock item = e.ClickedItem as TextBlock;
+            Stretch stretch;
+
+            switch (item.Text)
+            {
+                case "None":
+                    stretch = Stretch.None;
+                    break;
+
+                case "Stretch":
+                    stretch = Stretch.Fill;
+                    break;
+
+                case "Fit":
+                    stretch = Stretch.Uniform;
+                    break;
+
+                case "Fill":
+                    stretch = Stretch.UniformToFill;
+                    break;
+
+                default:
+                    Debug.WriteLine("Unknown popup option " + item.Text + "selected");
+                    return;
+            }
+            ForegroundImage.Stretch = BackgroundImage.Stretch = stretch;
+        }
+
+        private void SetBackground(Color color)
+        {
+            ForegroundImageGrid.Background = MainGrid.Background = new SolidColorBrush(color);
+        }
+
+        private async Task<BitmapImage> LoadImage(string id)
+        {
+            var bitmap = new BitmapImage();
+            using (var response = await _oneDrive.Client.Drive.Items[id].Content.Request().GetAsync())
+            {
+                if (response is MemoryStream)
+                {
+                    await bitmap.SetSourceAsync(((MemoryStream)response).AsRandomAccessStream());
+                }
+                else
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await response.CopyToAsync(memoryStream);
+                        memoryStream.Position = 0;
+                        await bitmap.SetSourceAsync(memoryStream.AsRandomAccessStream());
+                    }
+                }
+            }
+            return bitmap;
         }
     }
 }
